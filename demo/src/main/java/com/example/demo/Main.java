bfda0184933102eebc212c46d96c8c5e9cd20a81
José Antonio Vázquez <javazquez@uc-3-337.AluVedruna.local>
2024-09-18 17:25:57 +0200

    java leer fichero

--- demo/src/main/java/com/example/demo/Main.java
+++ demo/src/main/java/com/example/demo/Main.java
@@ -1,7 +1,19 @@
 package com.example.demo;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String rutaFichero = "C:\\Users\\javazquez\\Desktop\\Entorno Servidor\\demo\\tarea1.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaFichero))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
 }
